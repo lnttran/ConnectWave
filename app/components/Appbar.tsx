1bdd9449b5309cec3bc8897769c2884ad6664eab
SMUEric1127 <85500156+SMUEric1127@users.noreply.github.com>
2023-10-25 16:13:39 -0500

    ok

---
+++ app/components/Appbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignInButton from './SignInButton'
+
+export const Appbar = () => {
+  return (
+    <header className='flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow'>
+        <SignInButton/>
+    </header>
+  )
+}
