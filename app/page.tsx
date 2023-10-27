3fe0975ac167b281959b7e9671414ae7c2192d67
SMUEric1127 <85500156+SMUEric1127@users.noreply.github.com>
2023-10-27 16:54:41 -0500

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Image from 'next/image'
+"use client";
+import Iframe from "react-iframe";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 h-full">
+      <Iframe
+        url="https://yzpvu9cn.dora.run/"
+        width="100%"
+        height="100%"
+        display="block"
+        position="absolute"
+      />
+      {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -71,7 +79,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
-  )
+  );
 }
--- lib/singleton/stripe.ts
+++ lib/singleton/stripe.ts
@@ -1,11 +1,10 @@
 import { Stripe, loadStripe } from "@stripe/stripe-js";
 
-let stripePromise: Promise<Stripe | null>;
-const getStripe = () => {
-  if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
-  }
-  return stripePromise;
-};
-
-export default getStripe;
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  // https://github.com/stripe/stripe-node#configuration
+  apiVersion: "2022-11-15",
+  appInfo: {
+    name: "nextjs-with-stripe-typescript-demo",
+    url: "https://nextjs-with-stripe-typescript-demo.vercel.app",
+  },
+});
